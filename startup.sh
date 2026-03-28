6e76f02d16c5287cbb09d45534e341b1873f7b95
FX_SysTradeEA <chie@ogidani.com>
2026-03-28 14:51:57 +0900

    Add desktop shortcuts in startup.sh

--- startup.sh
+++ startup.sh
@@ -3,6 +3,11 @@ mkdir -p ~/.vnc
 echo "${VNC_PW:-vps12345}" | vncpasswd -f > ~/.vnc/passwd
 chmod 600 ~/.vnc/passwd
 
+# Desktop shortcuts
+mkdir -p /root/Desktop
+printf '[Desktop Entry]\nName=ヘルプ・使い方\nExec=xdg-open https://note.com/ogidanillc\nType=Application\nIcon=help-browser\nTerminal=false\n' > /root/Desktop/help.desktop
+chmod +x /root/Desktop/help.desktop
+
 vncserver :1 -geometry 1280x768 -depth 24 -localhost no &
 sleep 3
 
